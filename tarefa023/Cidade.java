cef4fd3d9db9577b91da4c4b807f7ab241e89dd4
vanessabrenda <31829323+vanessabrenda@users.noreply.github.com>
2021-11-04 08:43:17 -0300

    Create Cidade.java

---
+++ tarefa023/Cidade.java
@@ -0,0 +1,28 @@
+public class Cidade {
+    private String nome;
+    private Estado estado;
+
+    public Cidade() {
+    }
+
+    public Cidade(String nome, Estado estado) {
+        this.nome = nome;
+        this.estado = estado;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Estado getEstado() {
+        return this.estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+}
