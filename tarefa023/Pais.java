fbae83a15004c01865750047361747393cd5ec9a
vanessabrenda <31829323+vanessabrenda@users.noreply.github.com>
2021-11-04 08:55:22 -0300

    Create Pais.java

---
+++ tarefa023/Pais.java
@@ -0,0 +1,16 @@
+
+public class Pais {
+    private String nome;
+
+    public Pais(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
